7bfdefd859076356247da2eb1de15da009812fde
Parkis Utama <parkisutama@outlook.com>
2024-06-14 14:37:29 +0700

    change uniqueID to id

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   const data = [
-    { UniqueID: '123', Name: 'Pak Mamat', JenisPohon: 'Sengon Buto', ImageUrl: 'https://www.appsheet.com/template/gettablefileurl?appName=PENGUKURAN-31277156&tableName=DATAPENGUKURANBLOK06&fileName=%2FBLOK_06%2FBLOK_06_VISUAL_POHON%2F%2FIND2023LHT079147_79021-80100_ffd9de73.FOTO%20POHON.122112.jpg' },
-    { UniqueID: '456', Name: 'Pine', JenisPohon: 'Coniferous', ImageUrl: 'https://example.com/image2.jpg' },
-    { UniqueID: '789', Name: 'Maple', JenisPohon: 'Deciduous', ImageUrl: 'https://example.com/image3.jpg' },
+    { id: '123', Name: 'Pak Mamat', JenisPohon: 'Sengon Buto', ImageUrl: 'https://www.appsheet.com/template/gettablefileurl?appName=PENGUKURAN-31277156&tableName=DATAPENGUKURANBLOK06&fileName=%2FBLOK_06%2FBLOK_06_VISUAL_POHON%2F%2FIND2023LHT079147_79021-80100_ffd9de73.FOTO%20POHON.122112.jpg' },
+    { id: '456', Name: 'Pine', JenisPohon: 'Coniferous', ImageUrl: 'https://example.com/image2.jpg' },
+    { id: '789', Name: 'Maple', JenisPohon: 'Deciduous', ImageUrl: 'https://example.com/image3.jpg' },
   ];
 
   const urlParams = new URLSearchParams(window.location.search);
-  const uniqueID = urlParams.get('uniqueID');
+  const uniqueID = urlParams.get('id');
 
-  const matchingEntry = data.find(entry => entry.UniqueID === uniqueID);
+  const matchingEntry = data.find(entry => entry.id === id);
 
   const nameDisplay = document.getElementById('name-display');
   const jenisPohonDisplay = document.getElementById('jenis-pohon-display');
@@ -23,6 +23,6 @@ document.addEventListener('DOMContentLoaded', () => {
     // Set the src attribute of the hero image
     heroImage.src = matchingEntry.ImageUrl;
   } else {
-    errorMessage.textContent = 'No data found for this Unique ID.';
+    errorMessage.textContent = 'No data found for this ID.';
   }
 });
